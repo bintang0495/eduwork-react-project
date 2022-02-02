02e3e91da780e533b8ee3b4f964af2f280e47eca
bintang0495 <bintangmuhamadakbar@gmail.com>
2022-02-02 10:40:53 +0700

    Redux

---
+++ src/Materi/Redux/Counter.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decrementWithCheckingAction,
+  increment,
+} from '../../app/Features/Counter/actions';
+
+const Counter = () => {
+  let { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {/* <button onClick={() => dispatch({ type: 'DEC', value: 1 })}>-</button> */}
+      {/* <button onClick={() => dispatch(decrement(1))}>-</button> */}
+      <button onClick={() => dispatch(decrementWithCheckingAction(1))}>
+        -
+      </button>
+      <span>{count}</span>
+      {/* <button onClick={() => dispatch({ type: 'INC', value: 1 })}>+</button> */}
+      <button onClick={() => dispatch(increment(1))}>+</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/app/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import counterReducer from './Features/Counter/reducer';
+
+let rootReducers = combineReducers({
+  counter: counterReducer,
+});
+
+const composeEnchancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(
+  rootReducers,
+  composeEnchancers(applyMiddleware(thunk))
+);
+
+export default store;
